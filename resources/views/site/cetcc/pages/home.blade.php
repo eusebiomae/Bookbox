e707c9b430e7b2299c149a7a3a40f42cf8700629
mayitoae84 <mario.enriquez@gigapixel.com.br>
2021-09-20 23:36:04 -0300

    pagina Home finalizada!

--- resources/views/site/cetcc/pages/home.blade.php
+++ resources/views/site/cetcc/pages/home.blade.php
@@ -594,20 +594,20 @@
                         <!-- Product-->
                         <article class="product wow fadeInRight">
                             <div class="product-body">
-                                <img src="{{ url('assets/images/site/LogoMini Estilizada.png') }}" alt="" class="" style="width: 250px; margin-top: 16px;">
-                                <h3 class="product-title" style="margin-top: 15px;"><a href="single-product.html">Sou digital</a></h3>
+                                <img src="{{ url('assets/images/site/LogoMini Estilizada.png') }}" alt="" class="" style="margin-top: 23px; width: 250px;">
+                                <h3 class="product-title" style="margin-top: 5px;"><a href="single-product.html">Sou digital</a></h3>
                                 <div class="product-price-wrap">
                                     <div class="product-price" style="margin-bottom: 15px;"><h6 class="">R$ 39.90/mês</h6></div>
-                                    <div class="product-price">Receba um link para download em seu</div>
-                                    <div class="product-price"> e-mail.</div>
+                                    <div class="product-price">Receba um link para download</div>
+                                    <div class="product-price">  em seu e-mail.</div>
                                 </div>
                                 <div class="" style="margin-top: 15px;">
-                                    <p class="">- Sem fidelidade: você pode cancelar sem multa.</p>
-                                    <p class="">- Boleto bancário ou cartão de crédito.</p>
-                                    <p class="">- Receba a versão digital dos dois livros, esta oção não contempla os brindes.</p>
+                                    <p class="">- Sem fidelidade: sem multa.</p>
+                                    <p class="">- Boleto ou cartão de crédito.</p>
+                                    {{-- <p class="">- </p> --}}
                                 </div>
                             </div><span class="product-badge product-badge-new">Novo</span>
-                            <div class="product-button-wrap" style="margin-top: 50px;">
+                            <div class="product-button-wrap" style="margin-top: 25px;">
                                     <div class="button-wrap" data-caption-animate="fadeInUp" data-caption-delay="400"><a
                                         class="button button-lg button-shadow-4 button-secondary-2 button-zakaria"
                                         href="grid-shop.html">Assine Agora</a></div>
@@ -619,17 +619,17 @@
                         <article class="product wow fadeInRight">
                             <div class="product-body">
                                 <img src="{{ url('assets/images/site/01.png') }}" alt="" class="">
-                                <h3 class="product-title" style="margin-top: 15px;"><a href="single-product.html">Amo o cheirinho de livro</a></h3>
+                                <h3 class="product-title" style="margin-top: 5px;"><a href="single-product.html">Cheirinho de livro</a></h3>
                                 <div class="product-price-wrap">
                                     <div class="product-price" style="margin-bottom: 15px;"><h6 class="">R$ 89.90/mês</h6></div>
                                     <div class="product-price">Receba o seu box em casa.</div>
                                 </div>
                                 <div class="" style="margin-top: 15px;">
-                                    <p class="">- Sem fidelidade: você pode cancelar sem multa.</p>
-                                    <p class="">- Boleto bancário ou cartão de crédito.</p>
+                                    <p class="">- Sem fidelidade: sem multa.</p>
+                                    <p class="">- Boleto ou cartão de crédito.</p>
                                 </div>
                             </div><span class="product-badge product-badge-sale">Box</span>
-                            <div class="product-button-wrap" style="margin-top: 50px;">
+                            <div class="product-button-wrap" style="margin-top: 25px;">
                                     <div class="button-wrap" data-caption-animate="fadeInUp" data-caption-delay="400"><a
                                         class="button button-lg button-shadow-4 button-secondary-2 button-zakaria"
                                         href="grid-shop.html">Assine Agora</a></div>
@@ -641,7 +641,7 @@
                         <article class="product wow fadeInRight">
                             <div class="product-body">
                                 <img src="{{ url('assets/images/site/01.png') }}" alt="" class="">
-                                <h3 class="product-title" style="margin-top: 15px;"><a href="single-product.html">Quero todos os meses</a></h3>
+                                <h3 class="product-title" style="margin-top: 5px;"><a href="single-product.html">Quero todos os meses</a></h3>
                                 <div class="product-price-wrap">
                                     <div class="product-price" style="margin-bottom: 15px;"><h6 class="">R$ 79.90/mês</h6></div>
                                     <div class="product-price">Receba o seu box em casa.</div>
@@ -651,7 +651,7 @@
                                     <p class="">- Cartão de crédito.</p>
                                 </div>
                             </div><span class="product-badge product-badge-sale">Box</span>
-                            <div class="product-button-wrap" style="margin-top: 50px;">
+                            <div class="product-button-wrap" style="margin-top: 25px;">
                                     <div class="button-wrap" data-caption-animate="fadeInUp" data-caption-delay="400"><a
                                         class="button button-lg button-shadow-4 button-secondary-2 button-zakaria"
                                         href="grid-shop.html">Assine Agora</a></div>
@@ -663,7 +663,7 @@
                         <article class="product wow fadeInRight">
                             <div class="product-body">
                                 <img src="{{ url('assets/images/site/01.png') }}" alt="" class="">
-                                <h3 class="product-title" style="margin-top: 15px;"><a href="single-product.html">Quero o ano todo</a></h3>
+                                <h3 class="product-title" style="margin-top: 5px;"><a href="single-product.html">Quero o ano todo</a></h3>
                                 <div class="product-price-wrap">
                                     <div class="product-price" style="margin-bottom: 15px;"><h6 class="">R$ 69.90/mês</h6></div>
                                     <div class="product-price">Receba o seu box em casa.</div>
@@ -673,7 +673,7 @@
                                     <p class="">- Cartão de crédito.</p>
                                 </div>
                             </div><span class="product-badge product-badge-sale">Box</span>
-                            <div class="product-button-wrap" style="margin-top: 50px;">
+                            <div class="product-button-wrap" style="margin-top: 25px;">
                                     <div class="button-wrap" data-caption-animate="fadeInUp" data-caption-delay="400"><a
                                         class="button button-lg button-shadow-4 button-secondary-2 button-zakaria"
                                         href="grid-shop.html">Assine Agora</a></div>
