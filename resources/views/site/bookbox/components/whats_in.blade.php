8eff426d9c095df2330e7b79a28eb1337cd0f03c
mayitoae84 <mario.enriquez@gigapixel.com.br>
2021-10-06 11:39:53 -0300

    Idem

--- resources/views/site/bookbox/components/whats_in.blade.php
+++ resources/views/site/bookbox/components/whats_in.blade.php
@@ -1,6 +1,8 @@
 @foreach ($pageData->content as $item)
 <section id="whats_in" class="section section-xxl swiper-slide-pilars" style="background-image: url('{{$item['image_bg']}}');">
 	<div class="container">
+		<h3 class="text-transform-none wow text-align-center">{{$item->title_pt}}</h3>
+		<h6 class="text-transform-none wow text-justify">{{$item->subtitle_pt}}</h6>
 			<div class="row row-30 row-md-60 row-lg-70 justify-content-center align-items-md-center">
 					<div class="col-sm-8 col-md-5 col-xl-6">
 							<div class="inset-xl-right-20">
@@ -19,8 +21,6 @@
 					</div>
 
 					<div class="col-md-7 col-xl-6">
-							<h3 class="text-transform-none wow text-align-center">{{$item->title_pt}}</h3>
-							<h6 class="text-transform-none wow text-align-center">{{$item->subtitle_pt}}</h6>
 							<!-- Bootstrap collapse-->
 							<div class="card-group-custom card-group-corporate" id="accordion1" role="tablist"
 									aria-multiselectable="false">
